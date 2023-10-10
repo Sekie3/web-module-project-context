43967773e0e2af0592031beb7455420a53aea81c
Tyler <sekie0@gmail.com>
2023-10-10 12:26:14 -0600

    s10 m4

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,18 +1,21 @@
 import React from 'react';
-
-// Components
 import Item from './ShoppingCartItem';
+import { CartContext } from '../contexts/CartContext';
+import { useContext } from 'react';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
+	const { cart } = useContext(CartContext);
+
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{console.log(cart)}
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
